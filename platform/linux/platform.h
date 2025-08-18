09b6b4ce44c06aeeb61459cb329814f8cf9aab35
Latte72 <ryofuji2005@gmail.com>
2025-08-18 13:22:23 +0900

    Update: STEP 2

--- platform/linux/platform.h
+++ platform/linux/platform.h
@@ -1,24 +1,23 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+#include <pthread.h>
+#include <signal.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 /*
  * Memory
  */
 
 static inline void *
-memory_alloc(size_t size)
-{
-    return calloc(1, size);
+memory_alloc(size_t size) {
+  return calloc(1, size);
 }
 
 static inline void
-memory_free(void *ptr)
-{
-    free(ptr);
+memory_free(void *ptr) {
+  free(ptr);
 }
 
 /*
@@ -30,21 +29,33 @@ typedef pthread_mutex_t mutex_t;
 #define MUTEX_INITIALIZER PTHREAD_MUTEX_INITIALIZER
 
 static inline int
-mutex_init(mutex_t *mutex)
-{
-    return pthread_mutex_init(mutex, NULL);
+mutex_init(mutex_t *mutex) {
+  return pthread_mutex_init(mutex, NULL);
 }
 
 static inline int
-mutex_lock(mutex_t *mutex)
-{
-    return pthread_mutex_lock(mutex);
+mutex_lock(mutex_t *mutex) {
+  return pthread_mutex_lock(mutex);
 }
 
 static inline int
-mutex_unlock(mutex_t *mutex)
-{
-    return pthread_mutex_unlock(mutex);
+mutex_unlock(mutex_t *mutex) {
+  return pthread_mutex_unlock(mutex);
 }
+
+/*
+ * Interrupt
+ */
+
+#define INTR_IRQ_BASE (SIGRTMIN + 1)
+
+#define INTR_IRQ_SHARED 0x0001
+
+extern int intr_request_irq(unsigned int irq, int (*handler)(unsigned int irq, void *id), int flags, const char *name, void *dev);
+extern int intr_raise_irq(unsigned int irq);
+
+extern int intr_run();
+extern void intr_shutdown();
+extern int intr_init();
 
 #endif
